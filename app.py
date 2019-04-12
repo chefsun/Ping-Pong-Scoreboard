68431e879b7214f8c0f62fe70fb2ba0fde78eafd
Casey Sun <zsun.ece@gmail.com>
2019-04-11 20:25:24 -0600

    a simple screen

--- app.py
+++ app.py
@@ -18,8 +18,78 @@ class ScoreBoard:
     TODO: Impl this class with OpenCV GUI.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, bH=1080, bW=1920):
+
+        cv.startWindowThread()
+        cv.namedWindow('Scoreboard')
+
+        self.H = bH
+        self.W = bW
+        self.halfW = self.W // 2
+
+        # style
+        self.blue = (126, 77, 36)
+        self.red = (125, 126, 246)
+        self.font = cv.FONT_HERSHEY_SIMPLEX
+        self.fontsize = 24
+        self.fontthickness = 64
+
+        # BG
+        self.plain = np.zeros((self.H, self.W, 3), np.uint8)
+        self.plain[:, 0:self.halfW] = self.red
+        self.plain[:, self.halfW:] = self.blue
+
+        # score
+        self.LHS = 0
+        self.RHS = 0
+
+        self.curr = None
+        self._draw()
+
+    def _draw(self):
+        self.curr = self.plain.copy()
+        if self.LHS < 10:
+            self.curr = cv.putText(self.curr, str(self.LHS), (self.W//8, self.H*3//4), fontFace=self.font,
+                                   fontScale=self.fontsize, color=(255, 255, 255), thickness=self.fontthickness)
+        else:
+            self.curr = cv.putText(self.curr, str(self.LHS//10), (0, self.H*3//4), fontFace=self.font,
+                                   fontScale=self.fontsize, color=(255, 255, 255), thickness=self.fontthickness)
+            self.curr = cv.putText(self.curr, str(self.LHS % 10), (self.W//4, self.H*3//4), fontFace=self.font,
+                                   fontScale=self.fontsize, color=(255, 255, 255), thickness=self.fontthickness)
+        if self.RHS < 10:
+            self.curr = cv.putText(self.curr, str(self.RHS), (self.W*5//8, self.H*3//4), fontFace=self.font,
+                                   fontScale=self.fontsize, color=(255, 255, 255), thickness=self.fontthickness)
+        else:
+            self.curr = cv.putText(self.curr, str(self.RHS//10), (self.W//2, self.H*3//4), fontFace=self.font,
+                                   fontScale=self.fontsize, color=(255, 255, 255), thickness=self.fontthickness)
+            self.curr = cv.putText(self.curr, str(self.RHS % 10), (self.W*3//4, self.H*3//4), fontFace=self.font,
+                                   fontScale=self.fontsize, color=(255, 255, 255), thickness=self.fontthickness)
+
+    def update(self, winside):
+
+        if winside == 'L':
+            self.LHS += 1
+        elif winside == 'R':
+            self.RHS += 1
+        else:
+            raise ValueError("illegal update")
+
+        self._draw()
+
+        if self.LHS >= 11 or self.RHS >= 11:
+            _delta = abs(self.LHS - self.RHS)
+            if _delta >= 2:
+                self.curr = cv.putText(self.curr, str(winside+' Wins!'), (self.H//3, self.H*2//3), fontFace=self.font,
+                                       fontScale=self.fontsize//2, color=(0, 215, 255), thickness=self.fontthickness)
+                self.reset()
+
+    def display(self):
+        cv.imshow('Scoreboard', self.curr)
+        cv.waitKey(1)
+
+    def reset(self):
+        self.LHS = 0
+        self.RHS = 0
 
 
 class Ball:
@@ -68,6 +138,7 @@ class Game:
         self._calibrate()
 
         while self.stream.isOpened():
+            self.scoreboard.display()
             # get current frame
             success, frame = self.stream.read()
             if success:
